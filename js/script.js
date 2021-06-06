4cfd412ef05f801e7e36919c5188417c3d9bd2f2
terencecistudent <terencecistudent@users.noreply.github.com>
2021-06-07 00:37:25 +0100

    Portfolio complete but will keep modifying

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Burger Icon
+const hamburgerButton = document.getElementById('hamburger');
+const navList = document.getElementById('nav-list');
+
+const toggleButton = () => {
+    navList.classList.toggle('show');
+}
+
+hamburgerButton.addEventListener('click', toggleButton);
+
+// Navbar Active
+let selector = '.a-links';
+
+$(selector).on('click', function(){
+    $(selector).removeClass('active');
+    $(this).addClass('active');
+});
+
+// Start page at the top when reloaded
+history.scrollRestoration = 'manual';
+
+if (history.scrollRestoration) {
+    history.scrollRestoration = 'manual';
+} else {
+    window.onbeforeunload = () => {
+        window.scrollTo(0, 0);
+    }
+}
