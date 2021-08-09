a6a948cf968a02bf38c991b88bb23466f94b6ff7
terencecistudent <terencecistudent@users.noreply.github.com>
2021-08-09 14:49:33 +0100

    Styling changed to website, Bootstrap Scrollspy removed and floating back to top button added.

--- js/script.js
+++ js/script.js
@@ -17,31 +17,15 @@ if (history.scrollRestoration) {
   };
 }
 
-// Wrap every letter in a span
-var textWrapper = document.querySelector(".ml3");
-textWrapper.innerHTML = textWrapper.textContent.replace(
-  /\S/g,
-  "<span class='letter'>$&</span>"
-);
+// Scroll to top button
+let scrollToTopBtn = document.getElementById("scrollToTopBtn");
+let rootElement = document.documentElement;
 
-anime
-  .timeline({ loop: true })
-  .add({
-    targets: ".ml3 .letter",
-    opacity: [0, 1],
-    easing: "easeInOutQuad",
-    duration: 2250,
-    delay: (el, i) => 150 * (i + 1),
-  })
-  .add({
-    targets: ".ml3",
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000,
+const scrollToTop = () => {
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth",
   });
+};
 
-// Bootstrap Scrollspy
-var scrollSpy = new bootstrap.ScrollSpy(document.body, {
-  target: "#navbar-example",
-});
+scrollToTopBtn.addEventListener("click", scrollToTop);
